ff7583f7b4589103f55a5ea2ddfe431153012051
sachin-manore <sachin.manore84@gmail.com>
2024-01-18 15:54:51 +0530

    Update SQL_Script.sql

--- DataBase/SQL_Script.sql
+++ DataBase/SQL_Script.sql
@@ -1,95 +1,95 @@
-﻿USE [master]
+USE [master]
 GO
 /****** Object:  Database [footballmatch]    Script Date: 1/8/2024 11:35:32 AM ******/
-DROP DATABASE IF EXISTS [footballmatch];
-CREATE DATABASE [footballmatch]
+DROP DATABASE IF EXISTS [FootballMatch];
+CREATE DATABASE [FootballMatch]
  GO
-ALTER DATABASE [footballmatch] SET COMPATIBILITY_LEVEL = 140
+ALTER DATABASE [FootballMatch] SET COMPATIBILITY_LEVEL = 140
 GO
 IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
 begin
-EXEC [footballmatch].[dbo].[sp_fulltext_database] @action = 'enable'
+EXEC [FootballMatch].[dbo].[sp_fulltext_database] @action = 'enable'
 end
 GO
-ALTER DATABASE [footballmatch] SET ANSI_NULL_DEFAULT OFF 
+ALTER DATABASE [FootballMatch] SET ANSI_NULL_DEFAULT OFF 
 GO
-ALTER DATABASE [footballmatch] SET ANSI_NULLS OFF 
+ALTER DATABASE [FootballMatch] SET ANSI_NULLS OFF 
 GO
-ALTER DATABASE [footballmatch] SET ANSI_PADDING OFF 
+ALTER DATABASE [FootballMatch] SET ANSI_PADDING OFF 
 GO
-ALTER DATABASE [footballmatch] SET ANSI_WARNINGS OFF 
+ALTER DATABASE [FootballMatch] SET ANSI_WARNINGS OFF 
 GO
-ALTER DATABASE [footballmatch] SET ARITHABORT OFF 
+ALTER DATABASE [FootballMatch] SET ARITHABORT OFF 
 GO
-ALTER DATABASE [footballmatch] SET AUTO_CLOSE OFF 
+ALTER DATABASE [FootballMatch] SET AUTO_CLOSE OFF 
 GO
-ALTER DATABASE [footballmatch] SET AUTO_SHRINK OFF 
+ALTER DATABASE [FootballMatch] SET AUTO_SHRINK OFF 
 GO
-ALTER DATABASE [footballmatch] SET AUTO_UPDATE_STATISTICS ON 
+ALTER DATABASE [FootballMatch] SET AUTO_UPDATE_STATISTICS ON 
 GO
-ALTER DATABASE [footballmatch] SET CURSOR_CLOSE_ON_COMMIT OFF 
+ALTER DATABASE [FootballMatch] SET CURSOR_CLOSE_ON_COMMIT OFF 
 GO
-ALTER DATABASE [footballmatch] SET CURSOR_DEFAULT  GLOBAL 
+ALTER DATABASE [FootballMatch] SET CURSOR_DEFAULT  GLOBAL 
 GO
-ALTER DATABASE [footballmatch] SET CONCAT_NULL_YIELDS_NULL OFF 
+ALTER DATABASE [FootballMatch] SET CONCAT_NULL_YIELDS_NULL OFF 
 GO
-ALTER DATABASE [footballmatch] SET NUMERIC_ROUNDABORT OFF 
+ALTER DATABASE [FootballMatch] SET NUMERIC_ROUNDABORT OFF 
 GO
-ALTER DATABASE [footballmatch] SET QUOTED_IDENTIFIER OFF 
+ALTER DATABASE [FootballMatch] SET QUOTED_IDENTIFIER OFF 
 GO
-ALTER DATABASE [footballmatch] SET RECURSIVE_TRIGGERS OFF 
+ALTER DATABASE [FootballMatch] SET RECURSIVE_TRIGGERS OFF 
 GO
-ALTER DATABASE [footballmatch] SET  DISABLE_BROKER 
+ALTER DATABASE [FootballMatch] SET  DISABLE_BROKER 
 GO
-ALTER DATABASE [footballmatch] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+ALTER DATABASE [FootballMatch] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
 GO
-ALTER DATABASE [footballmatch] SET DATE_CORRELATION_OPTIMIZATION OFF 
+ALTER DATABASE [FootballMatch] SET DATE_CORRELATION_OPTIMIZATION OFF 
 GO
-ALTER DATABASE [footballmatch] SET TRUSTWORTHY OFF 
+ALTER DATABASE [FootballMatch] SET TRUSTWORTHY OFF 
 GO
-ALTER DATABASE [footballmatch] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+ALTER DATABASE [FootballMatch] SET ALLOW_SNAPSHOT_ISOLATION OFF 
 GO
-ALTER DATABASE [footballmatch] SET PARAMETERIZATION SIMPLE 
+ALTER DATABASE [FootballMatch] SET PARAMETERIZATION SIMPLE 
 GO
-ALTER DATABASE [footballmatch] SET READ_COMMITTED_SNAPSHOT OFF 
+ALTER DATABASE [FootballMatch] SET READ_COMMITTED_SNAPSHOT OFF 
 GO
-ALTER DATABASE [footballmatch] SET HONOR_BROKER_PRIORITY OFF 
+ALTER DATABASE [FootballMatch] SET HONOR_BROKER_PRIORITY OFF 
 GO
-ALTER DATABASE [footballmatch] SET RECOVERY FULL 
+ALTER DATABASE [FootballMatch] SET RECOVERY FULL 
 GO
-ALTER DATABASE [footballmatch] SET  MULTI_USER 
+ALTER DATABASE [FootballMatch] SET  MULTI_USER 
 GO
-ALTER DATABASE [footballmatch] SET PAGE_VERIFY CHECKSUM  
+ALTER DATABASE [FootballMatch] SET PAGE_VERIFY CHECKSUM  
 GO
-ALTER DATABASE [footballmatch] SET DB_CHAINING OFF 
+ALTER DATABASE [FootballMatch] SET DB_CHAINING OFF 
 GO
-ALTER DATABASE [footballmatch] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+ALTER DATABASE [FootballMatch] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
 GO
-ALTER DATABASE [footballmatch] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+ALTER DATABASE [FootballMatch] SET TARGET_RECOVERY_TIME = 60 SECONDS 
 GO
-ALTER DATABASE [footballmatch] SET DELAYED_DURABILITY = DISABLED 
+ALTER DATABASE [FootballMatch] SET DELAYED_DURABILITY = DISABLED 
 GO
-EXEC sys.sp_db_vardecimal_storage_format N'footballmatch', N'ON'
+EXEC sys.sp_db_vardecimal_storage_format N'FootballMatch', N'ON'
 GO
 ALTER DATABASE [footballmatch] SET QUERY_STORE = OFF
 GO
-USE [footballmatch]
+USE [FootballMatch]
 GO
 /****** Object:  Table [dbo].[fixture]    Script Date: 1/8/2024 11:35:32 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[fixture](
-	[id] [int] IDENTITY(1,1) NOT NULL,
-	[externalid] [int] NOT NULL,
-	[referee] [nvarchar](50) NULL,
-	[timezone] [nchar](10) NULL,
-	[date] [datetime] NULL,
-	[timestamp] [nvarchar](50) NULL,
- CONSTRAINT [PK_fixture] PRIMARY KEY CLUSTERED 
+CREATE TABLE [dbo].[Fixture](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[ExternalId] [int] NOT NULL,
+	[Referee] [nvarchar](50) NULL,
+	[Timezone] [nchar](10) NULL,
+	[Date] [datetime] NULL,
+	[Timestamp] [nvarchar](50) NULL,
+ CONSTRAINT [PK_Fixture] PRIMARY KEY CLUSTERED 
 (
-	[id] ASC
+	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
@@ -98,18 +98,18 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[league](
-	[leagueid] [int] IDENTITY(1,1) NOT NULL,
-	[fixtureid] [int] NULL,
-	[name] [nvarchar](50) NULL,
-	[country] [nvarchar](50) NULL,
-	[logo] [nvarchar](50) NULL,
-	[flag] [nvarchar](50) NULL,
-	[season] [nchar](10) NULL,
-	[round] [nchar](10) NULL,
- CONSTRAINT [PK_league] PRIMARY KEY CLUSTERED 
+CREATE TABLE [dbo].[League](
+	[LeagueId] [int] IDENTITY(1,1) NOT NULL,
+	[FixtureId] [int] NULL,
+	[Name] [nvarchar](50) NULL,
+	[Country] [nvarchar](50) NULL,
+	[Logo] [nvarchar](50) NULL,
+	[Flag] [nvarchar](50) NULL,
+	[Season] [nchar](10) NULL,
+	[Round] [nchar](10) NULL,
+ CONSTRAINT [PK_League] PRIMARY KEY CLUSTERED 
 (
-	[leagueid] ASC
+	[LeagueId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
@@ -118,14 +118,14 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[periods](
-	[periodsid] [int] IDENTITY(1,1) NOT NULL,
-	[fixtureid] [int] NULL,
-	[first] [nvarchar](50) NULL,
-	[second] [nvarchar](50) NULL,
- CONSTRAINT [PK_periods] PRIMARY KEY CLUSTERED 
+CREATE TABLE [dbo].[PerIods](
+	[PeriodsId] [int] IDENTITY(1,1) NOT NULL,
+	[FixtureId] [int] NULL,
+	[First] [nvarchar](50) NULL,
+	[Second] [nvarchar](50) NULL,
+ CONSTRAINT [PK_Periods] PRIMARY KEY CLUSTERED 
 (
-	[periodsid] ASC
+	[PeriodsId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
@@ -134,16 +134,16 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[score](
-	[scoreid] [int] IDENTITY(1,1) NOT NULL,
-	[teamid] [int] NULL,
-	[halftime] [int] NULL,
-	[fulltime] [int] NULL,
-	[extratime] [int] NULL,
-	[penalty] [int] NULL,
- CONSTRAINT [PK_score] PRIMARY KEY CLUSTERED 
+CREATE TABLE [dbo].[Score](
+	[ScoreId] [int] IDENTITY(1,1) NOT NULL,
+	[TeamId] [int] NULL,
+	[Halftime] [int] NULL,
+	[Fulltime] [int] NULL,
+	[Extratime] [int] NULL,
+	[Penalty] [int] NULL,
+ CONSTRAINT [PK_Score] PRIMARY KEY CLUSTERED 
 (
-	[scoreid] ASC
+	[ScoreId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
@@ -152,15 +152,15 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[status](
-	[statusid] [int] IDENTITY(1,1) NOT NULL,
-	[fixtureid] [int] NULL,
-	[long] [nvarchar](50) NULL,
-	[short] [nvarchar](50) NULL,
-	[elapsed] [nchar](10) NULL,
- CONSTRAINT [PK_status] PRIMARY KEY CLUSTERED 
+CREATE TABLE [dbo].[Status](
+	[StatusId] [int] IDENTITY(1,1) NOT NULL,
+	[FixtureId] [int] NULL,
+	[Long] [nvarchar](50) NULL,
+	[Short] [nvarchar](50) NULL,
+	[Elapsed] [nchar](10) NULL,
+ CONSTRAINT [PK_Status] PRIMARY KEY CLUSTERED 
 (
-	[statusid] ASC
+	[StatusId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
@@ -169,18 +169,18 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[teams](
-	[teamsid] [int] IDENTITY(1,1) NOT NULL,
-	[fixtureid] [int] NULL,
-	[teamlocation] [nvarchar](50) NULL,
-	[externalid] [int] NULL,
-	[name] [nvarchar](50) NULL,
-	[logo] [nvarchar](50) NULL,
-	[winner] [bit] NOT NULL,
-	[goals] [int] NULL,
- CONSTRAINT [PK_teams] PRIMARY KEY CLUSTERED 
+CREATE TABLE [dbo].[Teams](
+	[TeamsId] [int] IDENTITY(1,1) NOT NULL,
+	[FixtureId] [int] NULL,
+	[TeamLocation] [nvarchar](50) NULL,
+	[ExternalId] [int] NULL,
+	[Name] [nvarchar](50) NULL,
+	[Logo] [nvarchar](50) NULL,
+	[Winner] [bit] NOT NULL,
+	[Goals] [int] NULL,
+ CONSTRAINT [PK_Teams] PRIMARY KEY CLUSTERED 
 (
-	[teamsid] ASC
+	[TeamsId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
@@ -189,49 +189,49 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[venue](
-	[venueid] [int] IDENTITY(1,1) NOT NULL,
-	[fixtureid] [int] NOT NULL,
-	[externalvenueid] [int] NULL,
-	[name] [nvarchar](50) NULL,
-	[city] [nvarchar](50) NULL,
- CONSTRAINT [PK_venue] PRIMARY KEY CLUSTERED 
+CREATE TABLE [dbo].[Venue](
+	[VenueId] [int] IDENTITY(1,1) NOT NULL,
+	[FixtureId] [int] NOT NULL,
+	[ExternalVenueId] [int] NULL,
+	[Name] [nvarchar](50) NULL,
+	[City] [nvarchar](50) NULL,
+ CONSTRAINT [PK_Venue] PRIMARY KEY CLUSTERED 
 (
-	[venueid] ASC
+	[VenueId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-ALTER TABLE [dbo].[league]  WITH CHECK ADD  CONSTRAINT [FK_league_fixture] FOREIGN KEY([fixtureid])
-REFERENCES [dbo].[fixture] ([id])
+ALTER TABLE [dbo].[League]  WITH CHECK ADD  CONSTRAINT [FK_League_Fixture] FOREIGN KEY([FixtureId])
+REFERENCES [dbo].[Fixture] ([Id])
 GO
-ALTER TABLE [dbo].[league] CHECK CONSTRAINT [FK_league_fixture]
+ALTER TABLE [dbo].[League] CHECK CONSTRAINT [FK_League_Fixture]
 GO
-ALTER TABLE [dbo].[periods]  WITH CHECK ADD  CONSTRAINT [FK_periods_fixture] FOREIGN KEY([fixtureid])
-REFERENCES [dbo].[fixture] ([id])
+ALTER TABLE [dbo].[Periods]  WITH CHECK ADD  CONSTRAINT [FK_Periods_Fixture] FOREIGN KEY([FixtureId])
+REFERENCES [dbo].[Fixture] ([Id])
 GO
-ALTER TABLE [dbo].[periods] CHECK CONSTRAINT [FK_periods_fixture]
+ALTER TABLE [dbo].[Periods] CHECK CONSTRAINT [FK_Periods_Fixture]
 GO
-ALTER TABLE [dbo].[score]  WITH CHECK ADD  CONSTRAINT [FK_score_score] FOREIGN KEY([teamid])
-REFERENCES [dbo].[teams] ([teamsid])
+ALTER TABLE [dbo].[Score]  WITH CHECK ADD  CONSTRAINT [FK_Score_Score] FOREIGN KEY([TeamId])
+REFERENCES [dbo].[Teams] ([TeamsId])
 GO
-ALTER TABLE [dbo].[score] CHECK CONSTRAINT [FK_score_score]
+ALTER TABLE [dbo].[Score] CHECK CONSTRAINT [FK_Score_Score]
 GO
-ALTER TABLE [dbo].[status]  WITH CHECK ADD  CONSTRAINT [FK_status_fixture] FOREIGN KEY([fixtureid])
-REFERENCES [dbo].[fixture] ([id])
+ALTER TABLE [dbo].[Status]  WITH CHECK ADD  CONSTRAINT [FK_Status_Fixture] FOREIGN KEY([FixtureId])
+REFERENCES [dbo].[Fixture] ([Id])
 GO
-ALTER TABLE [dbo].[status] CHECK CONSTRAINT [FK_status_fixture]
+ALTER TABLE [dbo].[Status] CHECK CONSTRAINT [FK_Status_Fixture]
 GO
-ALTER TABLE [dbo].[teams]  WITH CHECK ADD  CONSTRAINT [FK_teams_fixture] FOREIGN KEY([fixtureid])
-REFERENCES [dbo].[fixture] ([id])
+ALTER TABLE [dbo].[Teams]  WITH CHECK ADD  CONSTRAINT [FK_Teams_Fixture] FOREIGN KEY([FixtureId])
+REFERENCES [dbo].[Fixture] ([Id])
 GO
-ALTER TABLE [dbo].[teams] CHECK CONSTRAINT [FK_teams_fixture]
+ALTER TABLE [dbo].[Teams] CHECK CONSTRAINT [FK_Teams_Fixture]
 GO
-ALTER TABLE [dbo].[venue]  WITH CHECK ADD  CONSTRAINT [FK_venue_fixture] FOREIGN KEY([fixtureid])
-REFERENCES [dbo].[fixture] ([id])
+ALTER TABLE [dbo].[Venue]  WITH CHECK ADD  CONSTRAINT [FK_Venue_Fixture] FOREIGN KEY([FixtureId])
+REFERENCES [dbo].[Fixture] ([Id])
 GO
-ALTER TABLE [dbo].[venue] CHECK CONSTRAINT [FK_venue_fixture]
+ALTER TABLE [dbo].[Venue] CHECK CONSTRAINT [FK_Venue_Fixture]
 GO
 USE [master]
 GO
-ALTER DATABASE [footballmatch] SET  READ_WRITE 
+ALTER DATABASE [FootballMatch] SET  READ_WRITE 
 GO
